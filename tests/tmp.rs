e77d2669687568324c429cb5549c5c3696164617
Nikita <groz.yt56@gmail.com>
2025-06-28 01:01:09 +0300

    BitTorrent protocol

--- tests/tmp.rs
+++ tests/tmp.rs
@@ -10,7 +10,7 @@ async fn test_1() {
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
 
-    let tor = torcli::bencode::Torrent::from_bytes(&buf).unwrap();
+    let tor = rutor::bencode::Torrent::from_bytes(&buf).unwrap();
 
     println!("torrent:");
     println!("announce: {}", tor.announce.as_str());
@@ -20,16 +20,16 @@ async fn test_1() {
     println!("info.files: {:#?}", tor.info.files.as_ref());
     println!();
 
-    let info_hash = torcli::bencode::InfoHash::from_bytes(&buf).unwrap();
+    let info_hash = rutor::bencode::InfoHash::from_bytes(&buf).unwrap();
 
     println!("info_hash: {}", info_hash.hex());
 
-    let peer_id = torcli::peer::PeerId::gen_new();
-    let handshake = torcli::proto::handshake::Handshake::from_parts(&info_hash, &peer_id);
+    let peer_id = rutor::peer::PeerId::gen_new();
+    let handshake = rutor::proto::handshake::Handshake::from_parts(&info_hash, &peer_id);
 
     println!("handshake: {}", String::from_utf8_lossy(handshake.bytes()));
 
-    let res = torcli::tracker::TrackerRequest::default()
+    let res = rutor::tracker::TrackerRequest::default()
         .with_info_hash(&info_hash)
         .with_peer_id(&peer_id)
         .with_port(8666)
@@ -38,14 +38,14 @@ async fn test_1() {
         .await
         .unwrap();
 
-    let peers = torcli::peer::Peers::from_bytes(&res.peers);
+    let peers = rutor::peer::Peers::from_bytes(&res.peers);
 
     println!();
     println!("{:#?}", peers);
     println!();
 
     for (i, p) in peers.iter().enumerate() {
-        if let Ok(mut conn) = torcli::peer::conn::PeerConn::connect(&p.socket_addr).await {
+        if let Ok(mut conn) = rutor::peer::conn::PeerConn::connect(&p.socket_addr).await {
             let h = conn.handshake(&handshake).await;
             if h.is_err() {
                 continue;
